a0fe6a608865b057d3b03c8db17efd34bcdd2eb9
hakan <hakanozer98@gmail.com>
2024-11-24 01:28:57 +0300

    Add Tamagui integration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,30 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { useColorScheme } from 'react-native'
+import { TamaguiProvider } from 'tamagui'
+import tamaguiConfig from '../../tamagui.config'
+import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme()
+
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
+
+  if (!loaded) {
+    return null
+  }
+
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
+  )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Button, Text } from "tamagui";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button>Home</Button>
     </View>
   );
 }
